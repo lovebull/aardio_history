dc835ae1da2f97380b4b33ecbbc5c0ab5ab63056
lovebull <1050266096@qq.com>
2021-05-31 12:37:35 +0800

    aardio v32.26.2 更新： --------------------------------------------------------------------------- 1、win.dlg.font 支持自动调用 logFont.stripPoint() 函数。

--- lib/electron/app/.build/aardio/index.d.ts
+++ lib/electron/app/.build/aardio/index.d.ts
@@ -2,7 +2,6 @@
 
 export = aardio;
 export as namespace aardio;
-type 
 declare const aardio: aardio.ExternalEx
 
 declare namespace aardio {
@@ -21,37 +20,52 @@ declare namespace aardio {
 
         /** 引用内置模块 */
         require<T = any>(moduleName: string): T;
+
         /** 引用内置 Electron 模块 */
         require(moduleName: 'electron'): typeof Electron;
-        /** 调用 aardio 函数(即使在aasdl初始化以前仍然可以调用rpc函数) */
+
+        /** 调用 aardio 函数 */
         xcall(method: string, ...args: any[]): Promise<any>;
-        /** 注册 aardio 可调用的JS函数，如果同一进程内注册多个同名回调，仅回传最后一次回调的返回值，非 aardio 环境自动忽略不执行 */
+
+        /** 注册 aardio 可调用的 JS 回调函数，如果同一进程内注册多个同名回调，仅回传最后一次回调的返回值 */
         on(event: string, listener: (...args: any[]) => void): ExternalEx;
 
         /** RPC 服务端断开连接或连接失败 */
         on(event: "close", onClose: () => void): ExternalEx; 
+
+        /** 注销 aardio 可调用的 JS 回调函数，如果省略 @listener 参数则注销所有使用该事件注册的 JS 回调函数 */
+        off(event: string, listener?: (...args: any[]) => void): ExternalEx;
 
         /** 
          * 在 aardio 模块以及RPC函数服务端已准备就绪后（如果当前是浏览器渲染进程则DOM已准备就绪），
          * 执行此回调(如果当前已经准备就绪就直接执行),这里注册的函数保证只会执行一次，重新连接后必须重新调用此函数注册。 
          */
         ready(listener: (win?: Electron.BrowserWindow) => void): ExternalEx; 
+
         /** 退出进程 */
         quit: () => void;
-        /** 拖动标题栏，非 aardio 环境自动忽略不执行 */
+
+        /** 拖动标题栏 */
         hitCaption: () => Promise<void>;
-        /** 点击关闭窗口按钮，非 aardio 环境自动忽略不执行 */
+
+        /** 点击关闭窗口按钮 */
         hitClose: () => Promise<void>;
-        /** 点击最小化按钮，非 aardio 环境自动忽略不执行 */
+
+        /** 点击最小化按钮 */
         hitMin: () => Promise<void>;
-        /** 点击最大化按钮，非 aardio 环境自动忽略不执行 */
+
+        /** 点击最大化按钮 */
         hitMax: () => Promise<boolean>;
-        /** 窗口是否最大化，非 aardio 环境自动忽略不执行 */
+
+        /** 窗口是否最大化 */
         isZoomed: () => Promise<boolean>;
+
         /** 读取本地数据,此函数默认未定义，请在aardio的external接口导出该函数 */
         get: <T = any>(key: string) => Promise<T>;
+
         /** 写入本地数据,此函数默认未定义，请在aardio的external接口导出该函数 */
         set: <T>(key: string, value?: T) => Promise<void>;
+
         /** 获取 electron 主进程全局变量，仅在 electron 环境中有效 */
         getGlobal<T = any>(sharedObjectName: string): T
     }
@@ -92,16 +106,22 @@ declare namespace aardio {
 
         /** aardio 模块以及 RPC 函数服务端是否已准备就绪（如果当前是浏览器渲染进程则 DOM 已准备就绪），在此之前RPC函数只能通过 xcall 调用 */
         isReady: boolean;
+
         /** aardio/rpc 服务器是否可用 */
         rpc: boolean;
+
         /** 当前是否在浏览器环境中运行，在 Electron 主进程运行为 false */
         browser: boolean;
+
         /** 当前是否在 Electron 环境中运行 */
         electron: boolean;
+
         /** 当前是否在 Electron 渲染进程中运行 */
         electronRenderer: boolean;
+
         /** 当前是否在 aardio 开发环境中运行，发布后为 false */
         studioInvoke: boolean;
+
         /** 当前 Electron 窗口句柄 */
         hwndElectron: number; 
         
@@ -120,20 +140,28 @@ declare namespace aardio {
          * 置入 aardio 资源目录则默认使用http:协议
          * */
         indexUrl: string;
+
         /** 启动参数 */
         args ?: { [key: string]: string;  }
+
         /** 主进程启动目录,发布后为asar文件路径 */
         appPath: string;
+
         /** RPC服务以及嵌入HTTP服务端口 */
         rpcPort: number;
+
         /** 创建主窗口使用的参数 */
         browserWindow: Electron.BrowserWindowConstructorOptions;
+
         /** 应用程序英文名称 */
         name: string;
+
         /** 窗口默认标题,可在aardio中创建electron.app的第一个参数中指定，或使用win.title指定 */
         title: string;
+
         /** 主窗口使用的图标路径 */
         icon: string;
+        
         /** 自定义启动令牌 */
         token?: string;
     }
